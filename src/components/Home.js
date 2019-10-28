e068fd2cb2ec6b4993aee73a2b53b6404fb49e19
Elena Perers <elena.perers@chasacademy.se>
2019-10-28 22:45:08 +0100

    Tried to get react-charthjs-2 to work

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import io from "socket.io-client";
+import { Line } from 'react-chartjs-2';
 
 
 
 const Home = () => {
     const [product, setProduct] = useState('');
     const [title, setTitle] = useState('');
+    const [graphData, setGraphData] = useState({});
 
     var apiURL = "";
     var socketUrl = "";
@@ -19,8 +21,55 @@ const Home = () => {
         socketUrl = 'http://localhost:3005';
     }
 
+    var dataSet = {}
+    var xVal = 0;
+
+    
 
     useEffect(() => {
+        const saveObjectData = ((response) => {
+            var datasets = [];
+            var colours = ["#9B4FE6", "#F35DEE", "#660033", "#6666FF", "#DC1493"];
+
+            for (let index = 0; index < response.length; index++) {
+                const object = response[index];
+                var dataset = {
+                    events: [],
+                    label: object.title,
+                    fill: false,
+                    lineTension: 0.1,
+                    backgroundColor: colours[index],
+                    borderColor: colours[index],
+                    borderCapStyle: 'butt',
+                    borderDash: [],
+                    borderDashOffset: 0.0,
+                    borderJoinStyle: 'miter',
+                    pointBorderColor: colours[index],
+                    pointBackgroundColor: '#fff',
+                    pointBorderWidth: 1,
+                    pointHoverRadius: 5,
+                    pointHoverBackgroundColor: colours[index],
+                    pointHoverBorderColor: colours[index],
+                    pointHoverBorderWidth: 2,
+                    pointRadius: 1,
+                    pointHitRadius: 10,
+                    data: [object.startingPoint, object.startingPoint]
+                }
+                datasets.push(dataset);
+            }
+            dataSet =
+                {
+                    labels: ['-2', '-1'],
+                    datasets: datasets
+                };
+            setGraphData({
+                labels: ['-2', '-1'],
+                datasets: datasets
+            })
+            console.log("datasets", dataSet);
+            
+        });
+
         const fetchData = async () => {
             const result = await fetch(apiURL)
                 .then(res => res.json())
@@ -29,18 +78,41 @@ const Home = () => {
                 });
             setTitle(result.title);
             setProduct(result.products);
+            saveObjectData(result.products);
         };
         fetchData();
         var socket = io(socketUrl)
         socket.on('stocks', (products) => {
-            console.log(products);
             setProduct(products);
+            dataSet.labels.push(xVal.toString());
+
+            if (dataSet.labels.length > 30) {
+                dataSet.labels.shift();
+            }
+
+            for (let index = 0; index < products.length; index++) {
+                const product = products[index];
+                dataSet.datasets[index].data.push(product.startingPoint)
+                if (dataSet.datasets[index].data.length > 30) {
+                    dataSet.datasets[index].data.shift();
+                }
+            }
+            xVal++;
+            console.log("dataSet: ", dataSet);
+            setGraphData(dataSet);
         });
     }, [apiURL, socketUrl]);
 
     return (
         <main>
+            {console.log("draw DataSet:", graphData)}
             <h2>{title}</h2>
+            <Line
+                data={graphData}
+                width={500}
+                height={300}
+                options={{ maintainAspectRatio: true }}
+            />
             <section className="product-section">
                 {product ?
                     product.map((item, i) => {
@@ -48,7 +120,7 @@ const Home = () => {
                             <img src={item.imagePath} alt={item.title}></img>
                             <h3>{item.title}</h3>
                             <p>{item.description}</p>
-                            <p className="price">Pris: {Math.round(item.startingPoint * 100)/100}</p>
+                            <p className="price">Pris: {Math.round(item.startingPoint * 100) / 100}</p>
                             <Link to={{
                                 pathname: "/Buy",
                                 state: {
